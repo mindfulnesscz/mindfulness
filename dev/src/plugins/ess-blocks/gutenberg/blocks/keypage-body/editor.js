e001d7b4168608c6679ff318d903239dd3ded230
Jakub Runda <jakub@webmajstr.cz>
2022-02-23 00:43:27 +0100

    col AOS feature added, BackgroundVideo for Keypage Header and Color palette for Keypage Body background

--- dev/src/plugins/ess-blocks/gutenberg/blocks/keypage-body/editor.js
+++ dev/src/plugins/ess-blocks/gutenberg/blocks/keypage-body/editor.js
@@ -1,54 +1,107 @@
 /*jshint esversion: 6 */
 
-const {registerBlockType} = wp.blocks;
-const {InnerBlocks} = wp.blockEditor;
+const { registerBlockType } = wp.blocks;
 
+const {
+  InspectorControls,
+
+  PanelColorSettings,
+
+  InnerBlocks,
+
+  withColors,
+
+  getColorClassName,
+} = wp.blockEditor;
+
+const { PanelBody, PanelRow, SelectControl } = wp.components;
+
+const { Fragment } = wp.element;
 
 const block = registerBlockType('mindfulness-blocks/keypage-body', {
-    title: 'Keypage Body',
-    icon: 'format-image',
-    category: 'layout',
+  title: 'Keypage Body',
+  icon: 'format-image',
+  category: 'layout',
 
-    attributes: {
-    },
+  attributes: {
+  },
 
-    edit(props) {
+  edit: withColors('backgroundColor')((props) => {
 
-        const { className } = props;
+    const {
+      className,
+      attributes,
+      setAttributes,
+      backgroundColor,
+      setBackgroundColor,
+    } = props;
 
-        let classes = className + ' ' + 'ess-main mindfulness-wp-block';
-        console.log ( classes );
-        console.log ( className );
-        console.log (' ------------------------------ ttttttttttttttttttttttttttttttttttttt ');
-        return ([
-
-
-        
-        <main className={ classes } >
-             <div class="container">
-
-                <InnerBlocks />
-
-            </div>
-        </main>
-        ]);
-    },
-
-    save(props) {
-
-        const {setAttributes, attributes, className} = props;
-
-        return (
-            <main className={className} >
-                <div class="container">
+    const colorBlockClasses = (
+      'container ess-color-block-bkg' +
+      ' ' +
+      (backgroundColor.class || '')
+    ).trim();
 
 
-			        <InnerBlocks.Content />
+    let classes = className + ' ' + 'ess-main mindfulness-wp-block';
+    console.log(classes);
+    console.log(className);
+    console.log(' ------------------------------ ttttttttttttttttttttttttttttttttttttt ');
+    return ([
 
-                </div>
-            </main>
-        );
-    }
+      <Fragment key={"keypage-body-" + Math.round(Math.random() * 10000)}>
+        <InspectorControls>
+          <PanelBody title={'Keypage Body'} initalOpen={true}>
+            <PanelRow>
+              <PanelColorSettings
+                title={'Choose Color'}
+                colorSettings={[
+                  {
+                    value: backgroundColor.color,
+
+                    onChange: setBackgroundColor,
+
+                    label: 'Background Color',
+                  },
+                ]}
+              ></PanelColorSettings>
+            </PanelRow>
+          </PanelBody>
+        </InspectorControls>
+      </Fragment>,
+
+      <main className={classes + ' ' + colorBlockClasses} >
+        <div class={'container'}>
+
+          <InnerBlocks />
+
+        </div>
+      </main>
+    ]);
+  }),
+
+  save(props) {
+
+    const { attributes, className } = props;
+    const { backgroundColor } = attributes;
+
+    const colorBlockClasses = (
+      'ess-color-block-bkg' +
+      ' ' +
+      (getColorClassName('background-color', backgroundColor) || '')
+    ).trim();
+
+    return (
+      <main className={className + ' ' + colorBlockClasses} >
+        <div class={'container'}>
+
+
+          <InnerBlocks.Content />
+
+        </div>
+      </main>
+    );
+  }
 });
 
-export {block}
+export { block }
