3c14017e182af50cfbfebae8da4572bb02fa0c2d
jakub Runda <jakub@webmajstr.cz>
2022-04-28 18:19:13 +0200

    redesign to 1.2. started

--- template-parts/navigation/navigation-mobile-banner.php
+++ template-parts/navigation/navigation-mobile-banner.php
@@ -39,7 +39,7 @@
   <ul id="ess-home-banner-classic" class="mind-slider">
     <li class="slide ess-cube-banner-item">
       <a href="https://alsimcloud.com">
-        <img class="hbc-banner-image" width="705" height="1386" src="<?php echo home_url() . '/ess-media/home-banner/220312_repair/hbc-repair-banner-mobile.webp'; ?>">
+        <img loading="lazy" class="hbc-banner-image" width="705" height="1386" src="<?php echo home_url() . '/ess-media/home-banner/220312_repair/hbc-repair-banner-mobile.webp'; ?>">
       </a>
     </li>
   </ul>
