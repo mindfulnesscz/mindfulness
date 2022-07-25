c49c07e6cec6149c3eb928d21d652b4bfc0a2659
Jakub Runda <jakub@webmajstr.cz>
2022-07-25 10:11:59 +0200

    push before switch to main

--- page-home.php
+++ page-home.php
@@ -15,61 +15,8 @@
 
 get_header(); ?>
 
-<header id="ess-header">
-  <div id="ess-home-header">
-    <div id="ess-intro" class="container">
-      <div class="row start-xs middle-lg">
-        <div class="col col-xs-12 col-lg-4 col-xs-offset-0 col-lg-offset-3">
-          <div class="row no-margin no-padding lg-xs">
-            <div class="col-xs-12 col-lg-4 no-padding no-margin text-center">
-              <div id="scene">
-                <div id="rotator">
-                  <div id="cube">
-                    <div id="front"></div>
-                    <div id="back"></div>
-                    <div id="left"></div>
-                    <div id="right"></div>
-                    <div id="top"></div>
-                    <div id="bottom"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-xs-12 col-lg-8 no-padding">
-              <h1 id="logo-text">ess</h1>
-            </div>
-          </div>
-        </div>
-        <div class="col col-xs-12 col-lg-5">
-          <h1 id="ess-header-claim">
-            <span>painting the future</span><br>
-            <span> of advanced</span><br>
-            <span>technology</span>
-          </h1>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>
-
-
-
-
-
-<!--   BANNER  =======================================================================================================================-->
-
-<?php
-
-get_template_part('template-parts/navigation/navigation', 'mobile-banner-repair');
-
-?>
-
-
 
 <!--   MAIN  =========================================================================================================================-->
-
-
-
 
 
 <?php
